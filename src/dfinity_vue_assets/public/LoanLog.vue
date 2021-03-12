c65afacd1501c52a8e7b7f3d579dc34b38055bf5
Stéphane Kahn <stephane.kahn@mediatransports.com>
2021-03-12 16:31:52 +0100

    blah

--- src/dfinity_vue_assets/public/LoanLog.vue
+++ src/dfinity_vue_assets/public/LoanLog.vue
@@ -1,4 +1,11 @@
-<template>
+<style>
+  .borrow {
+    background-color: lightcoral;
+  }
+  .unborrow {
+    background-color: lightgreen;
+  }
+</style><template>
     <v-data-table
       ref="stock-table"
       :items="items"
@@ -8,15 +15,14 @@
       :loading="busy"
       loading-text="Chargement de l'historique en cours..."
       sort-by="loanId"
-      sort-desc="true"
-    >
-     <template v-slot:item.begin="{ item }">
-      <span>{{ formatDate(item.begin) }}</span>
-    </template>
-     <template v-slot:item.end="{ item }">
-      <span>{{ formatDate(item.end) }}</span>
-    </template>
-    
+      :item-class="itemRowBackground"
+    >        
+      <template v-slot:item.begin="{ item }">
+        <span>{{ formatDate(item.begin) }}</span>
+      </template>
+      <template v-slot:item.end="{ item }">
+        <span>{{ formatDate(item.end) }}</span>
+      </template>
     </v-data-table>
 </template>
 
@@ -38,24 +44,42 @@ export default {
     };
   },
 
+  mounted() {
+    this.recursiveGetAllItems();
+  },
   created() {
-    this.busy = true;
-    this.getAllItems();
+
   },
   methods: {
     formatDate(date) {
       return (!date || date == "") ? "N/A" : new Date(date/1000000).toLocaleString();
     },
     getAllItems() {
+      this.busy = true;
       stock.getAllLoans().then((items) => {
         console.log(items);
         this.items = items;
         this.busy = false;
       });
+      //setTimeout(this.getAllItems(), 10000);
     },
     isAvailable(item) {
       return !item.borrower || item.borrower == "";
     },
+    itemRowBackground: function (item) {
+      return (!item.end || item.end == "") ? 'borrow' : 'unborrow'
+    },
+
+    recursiveGetAllItems() {
+      this.busy = true;
+      stock.getAllLoans().then((items) => {
+        console.log(items);
+        this.items = items;
+        this.busy = false;
+      });
+      
+      setTimeout(recursiveGetAllItems(), 5000);
+    }
   },
 };
 </script>
